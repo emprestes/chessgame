0d2e0b1612cddc7b79cc84c70e05ff9acf36e8c8
Prestes, E. M <emprestes@gmail.com>
2016-09-24 19:08:19 +0100

    #39

--- core/src/main/java/chessgame/domain/Piece.java
+++ core/src/main/java/chessgame/domain/Piece.java
@@ -1,7 +1,10 @@
 package chessgame.domain;
 
+import chessgame.domain.model.BoardPosition;
+
 import java.io.Serializable;
 
 public interface Piece extends Serializable {
 
+    BoardPosition getAvailablePosition();
 }
