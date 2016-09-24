1122346d1c7592cabf854e4bb1bcc7c1e0b8921c
Prestes, E. M <emprestes@gmail.com>
2016-09-24 23:26:47 +0100

    #39

--- core/src/main/java/chessgame/domain/Piece.java
+++ core/src/main/java/chessgame/domain/Piece.java
@@ -1,10 +1,25 @@
 package chessgame.domain;
 
+import chessgame.domain.model.Board;
 import chessgame.domain.model.BoardPosition;
+import chessgame.domain.model.PieceColor;
 
 import java.io.Serializable;
+import java.util.Set;
 
 public interface Piece extends Serializable {
 
-    BoardPosition getAvailablePosition();
+    Board getBoard();
+
+    PieceColor getColor();
+
+    Piece setColor(PieceColor color);
+
+    boolean canCapture(String position);
+
+    boolean canMove(String position);
+
+    Piece move(String position);
+
+    Set<BoardPosition> getAvailablePosition();
 }
