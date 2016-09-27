f76c27c3fb3b156e9f42c12ad3b8037685c18013
Prestes, E. M <emprestes@gmail.com>
2016-09-27 23:39:01 +0100

    #39

--- core/src/test/java/chessgame/domain/model/RookTest.java
+++ core/src/test/java/chessgame/domain/model/RookTest.java
@@ -1,5 +1,16 @@
 package chessgame.domain.model;
 
+import org.junit.Test;
+
 public class RookTest {
 
+    @Test
+    public void getAvailablePositionsOK() throws Exception {
+        // TODO Test here.
+    }
+
+    @Test
+    public void getAvailablePositionsNOK() throws Exception {
+        // TODO Test here.
+    }
 }
