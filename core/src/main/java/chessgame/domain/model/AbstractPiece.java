8069ad8d473fd608dc6c9b7a19338e44b3692540
Prestes, E. M <emprestes@gmail.com>
2016-09-28 12:35:15 +0100

    #13 Implementation of AbstractPiece (Position).

--- core/src/main/java/chessgame/domain/model/AbstractPiece.java
+++ core/src/main/java/chessgame/domain/model/AbstractPiece.java
@@ -4,4 +4,13 @@ import chessgame.domain.Piece;
 
 abstract class AbstractPiece implements Piece {
 
+    private BoardPosition position;
+
+    public BoardPosition getPosition() {
+        return position;
+    }
+
+    public void setPosition(BoardPosition position) {
+        this.position = position;
+    }
 }
