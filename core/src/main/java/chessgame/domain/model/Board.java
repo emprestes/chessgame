d1429d666d6a6feba6940ef577f1a4cde7bb5302
Prestes, E. M <emprestes@gmail.com>
2016-09-28 14:31:26 +0100

    #13 Implementation of Board (Players).

--- core/src/main/java/chessgame/domain/model/Board.java
+++ core/src/main/java/chessgame/domain/model/Board.java
@@ -1,9 +1,27 @@
 package chessgame.domain.model;
 
 import chessgame.domain.Piece;
+import chessgame.domain.Player;
 
 import java.util.TreeMap;
 
 public class Board extends TreeMap<BoardPosition, Piece> {
 
+    private final Player white;
+
+    private final Player black;
+
+    public Board() {
+        super();
+
+        this.white = new HumanPlayer();
+        this.black = new HumanPlayer();
+
+        init();
+    }
+
+    public Board init() {
+        // TODO Implementation here.
+        return this;
+    }
 }
