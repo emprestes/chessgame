1044ce9c5c87a1c4e407589e7028ca46f5a48aa0
Prestes, E. M <emprestes@gmail.com>
2016-09-28 13:24:25 +0100

    #13 Implementation of PieceFactory.

--- core/src/main/java/chessgame/domain/PieceFactory.java
+++ core/src/main/java/chessgame/domain/PieceFactory.java
@@ -1,5 +1,20 @@
 package chessgame.domain;
 
+import chessgame.domain.exception.PieceException;
+import chessgame.domain.model.Board;
+import chessgame.domain.model.PieceColor;
+
 final class PieceFactory {
 
+    private PieceFactory() {
+        super();
+    }
+
+    static Piece create(Board board, PieceColor color, Class<? extends Piece> _class) throws PieceException {
+        try {
+            return _class.getConstructor(Board.class, PieceColor.class).newInstance(board, color);
+        } catch (ReflectiveOperationException cause) {
+            throw new PieceException(cause);
+        }
+    }
 }
