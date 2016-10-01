bf72b62c769e0a47ff6b6935cbbee2577922da4c
Prestes, E. M <emprestes@gmail.com>
2016-10-01 16:37:51 +0200

    #13 Implementation of Chess.

--- app-fx/src/main/java/chessgame/app/Chess.java
+++ app-fx/src/main/java/chessgame/app/Chess.java
@@ -1,5 +1,25 @@
 package chessgame.app;
 
-public class Chess {
+import chessgame.app.fx.util.FXMLHelper;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import static chessgame.app.fx.controller.BoardController.VIEW_TITLE;
+import static chessgame.app.fx.controller.BoardController.VIEW_URL;
+
+public class Chess extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene board = FXMLHelper.createScene(primaryStage, VIEW_TITLE, VIEW_URL);
+
+        primaryStage.setScene(board);
+        primaryStage.setResizable(Boolean.FALSE);
+        primaryStage.show();
+    }
 }
