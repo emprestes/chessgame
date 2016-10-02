9d9ab89f2ee3ca2a8251374ee4a4f6988a3dd967
Prestes, E. M <emprestes@gmail.com>
2016-10-02 14:01:59 +0200

    #13 Implementation of BoardController (BoardView).

--- app-fx/src/main/java/chessgame/app/fx/controller/BoardController.java
+++ app-fx/src/main/java/chessgame/app/fx/controller/BoardController.java
@@ -1,9 +1,14 @@
 package chessgame.app.fx.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
 public class BoardController extends AbstractController {
 
     public static final String VIEW_TITLE = "Chess Game";
 
     public static final String VIEW_URL = String.format(VIEW_PACKAGE, "BoardView");
 
+    @FXML
+    private GridPane boardView;
 }
